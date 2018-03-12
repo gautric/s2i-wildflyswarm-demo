0c72ed6f784145a1ead867105c6e4ec70fa1ada8
gautric <gautric@redhat.com>
2018-03-12 14:31:31 +0100

    Update and install metrics

--- src/main/java/net/a/g/s2i/wildflyswarm/demo/rest/HelloWorldEndpoint.java
+++ src/main/java/net/a/g/s2i/wildflyswarm/demo/rest/HelloWorldEndpoint.java
@@ -1,18 +1,23 @@
 package net.a.g.s2i.wildflyswarm.demo.rest;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.metrics.annotation.Counted;
 
 @ApplicationScoped
-@Path("/hello")
+@Path("/")
 public class HelloWorldEndpoint {
 
 	@GET
+	@Path("/hello")
 	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("Hello from WildFly Swarm!").build();
+	@Counted(monotonic = true)
+	public Response getHello() {
+		return Response.ok("Hello from WildFly ").build();
 	}
+
 }
