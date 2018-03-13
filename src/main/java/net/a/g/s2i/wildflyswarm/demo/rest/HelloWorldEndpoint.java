9d7e66f3bd550668c7badcc0f298502c6de8c1b9
gautric <gautric@redhat.com>
2018-03-13 22:46:51 +0100

    add index.html

--- src/main/java/net/a/g/s2i/wildflyswarm/demo/rest/HelloWorldEndpoint.java
+++ src/main/java/net/a/g/s2i/wildflyswarm/demo/rest/HelloWorldEndpoint.java
@@ -1,9 +1,14 @@
 package net.a.g.s2i.wildflyswarm.demo.rest;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.DefaultValue;
@@ -23,15 +28,35 @@ public class HelloWorldEndpoint {
 
 	Logger LOG = Logger.getLogger(HelloWorldEndpoint.class);
 
+	// @GET
+	// @Produces("text/html")
+	// @Counted(monotonic = true)
+	// public Response getHome() {
+	// LOG.info("Call /");
+	// LOG.debug("Call / en mode debug");
+	// return Response
+	// .ok("<html><a href=\"/hello\">hello</a><br/><a
+	// href=\"/secret\">secret</a><br/><a
+	// href=\"/configmap?file=/data/config.txt\">configMap</a><br/></html>")
+	// .build();
+	// }
+
 	@GET
 	@Produces("text/html")
 	@Counted(monotonic = true)
-	public Response getHome() {
+	public Response getHome() throws IOException {
 		LOG.info("Call /");
 		LOG.debug("Call / en mode debug");
+		InputStream is = this.getClass().getResourceAsStream("/index.html");
 		return Response
-				.ok("<html><a href=\"/hello\">hello</a><br/><a href=\"/secret\">secret</a><br/><a href=\"/configmap?file=/data/config.txt\">configMap</a><br/></html>")
+				.ok(convert(is))
 				.build();
+	}
+
+	public String convert(InputStream inputStream) throws IOException {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+			return br.lines().collect(Collectors.joining(System.lineSeparator()));
+		}
 	}
 
 	@GET
